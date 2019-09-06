a4b9195462dbb777c34dc9370416becee774406a
singh95ravindra <singh95ravindra@email.com>
2019-09-06 11:24:46 +0530

    added method to delete user.

--- src/main/java/com/practice/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/practice/rest/webservices/restfulwebservices/user/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -55,4 +56,10 @@ public class UserResource {
 
 		return ResponseEntity.created(location).build();
 	}
+
+	@DeleteMapping("/users/{id}")
+	public void deleteUser(@PathVariable long id) {
+		userRepository.deleteById(id);
+	}
+
 }
